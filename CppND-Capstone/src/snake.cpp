903eb013dac86056658e6bb986ecd3f6364f4efc
cjonesnine2 <138345643+cjonesnine2@users.noreply.github.com>
2023-08-21 21:18:09 -0700

    Update snake.cpp

--- CppND-Capstone/src/snake.cpp
+++ CppND-Capstone/src/snake.cpp
@@ -2,20 +2,6 @@
 #include <cmath>
 #include <iostream>
 
-/*
-void Snake::Update() {
-  SDL_Point prev_cell{
-      static_cast<int>(head_x),
-      static_cast<int>(
-          head_y)};  // We first capture the head's cell before updating.
-  UpdateHead();
-  SDL_Point current_cell{
-      static_cast<int>(head_x),
-      static_cast<int>(head_y)};  // Capture the head's cell after updating.
-}
-*/
-
-
 void Snake::Update() {
   switch (direction) {
     case Direction::kNone:
